185c7b46e8fab37138dcf9a5fa11641f781d8472
Stephora <stephora.casimir@escola.pr.gov.br>
2025-09-09 15:09:31 +0000

    Stephora

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(" O servidor está funcionando na porta " + port);
+});
